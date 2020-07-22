18990e2155ef08527bdd8a00b1ceb1a189a09182
stoic-park <demige79@gmail.com>
2020-07-22 19:18:23 +0900

    :wrench: Fix: Link css fix
    - 링크 호버시 텍스트를 띄우도록 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,21 +34,25 @@ const IndexPage = () => (
         </div>
         <ul className="homeLinkContainer">
           <li>
+            <span className="homeLinkTitle">github</span>
             <Link to="http://bitly.kr/YspLJtlIKzB" target="_blank">
               <FaGithub size="32" />
             </Link>
           </li>
           <li>
+            <span className="homeLinkTitle">medium</span>
             <Link to="http://bitly.kr/gbgN5IFJuCw" target="_blank">
               <FaMedium size="32" />
             </Link>
           </li>
           <li>
+            <span className="homeLinkTitle">email</span>
             <a href="mailto:demige79@gmail.com">
               <MdEmail size="32" />
             </a>
           </li>
           <li>
+            <span className="homeLinkTitle">resume</span>
             <Link
               to="https://sharebook-gifs.s3.ap-northeast-2.amazonaws.com/%EC%9D%B4%EB%A0%A5%EC%84%9C_%EB%B0%95%EC%84%B1%ED%83%9D.pdf"
               target="_blank"
