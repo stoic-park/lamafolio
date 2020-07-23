bfebe3f84e661a283d2ede3633f5a923804e7d05
stoic-park <demige79@gmail.com>
2020-07-24 01:21:47 +0900

    :wrench: Fix: About/Project CSS fix
    - about page 이면가시성 적용
    - project page max-height 이용

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,66 +4,54 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// material-ui
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
+// material-ui & CSS
 import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
 import "./about.css"
 
 const About = () => (
   <Layout>
     <div className="aboutContainer">
-      <Card className="aboutCardContainer">
-        <CardMedia
-          className="aboutCardMedia"
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        {/* <CardContent className="cardContent">
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            Github
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions> */}
-      </Card>
-      <Card className="aboutCardContainer">
-        <CardMedia
-          className="aboutCardMedia"
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        {/* <CardContent className="cardContent">
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            Github
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions> */}
-      </Card>
+      <div className="aboutIntroContainer">
+        <div className="aboutCard">
+          <div className="aboutCard__side aboutCard__side--Front">
+            <div className="aboutCard__box">
+              <p class="card__subject">ABOUT</p>
+              <p class="card__title">ME</p>
+            </div>
+          </div>
+          <div className="aboutCard__side aboutCard__side--Back">
+            <div className="aboutCard__box">
+              <p>사람을 좋아하고 사람들이 좋아해주기를 바라는</p>
+              <p>혼자보다는 함께 무언가를 이루고 싶어하는</p>
+              <p>맡은 일은 무슨 일이 있어도 해야하고</p>
+              <p>부족하다는 것을 부끄럽게 여기기 보다는 성장의 기회로 삼는</p>
+              <p>나의 관점을 앞세우기보다도 상대방의 의견을 먼저 생각하는</p>
+              <p>깔끔한 디자인과 참신하고 재미있는 아이디어를 좋아하는</p>
+              <p>
+                재미있는 아이디어를 기획부터 완성까지 하는 것을 너무나 좋아라
+                하는
+              </p>
+              <p>개발자입니다</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="aboutIntroContainer">
+        <div className="aboutCard">
+          <div className="aboutCard__side aboutCard__side--Front">
+            <div className="aboutCard__box">
+              <p class="card__subject">SKILLS</p>
+              <p class="card__title">&TOOLS</p>
+            </div>
+          </div>
+          <div className="aboutCard__side aboutCard__side--Back">
+            <div className="aboutCard__box">
+              <p>JavaScript</p>
+              <p>React, React Native, Redux</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Layout>
 )
--- src/pages/project.js
+++ src/pages/project.js
@@ -18,78 +18,49 @@ let projects = []
 const Project = () => (
   <Layout>
     <div className="projectContainer">
-      <Card className="cardContainer">
-        <CardMedia
-          className="cardMedia"
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        <CardContent className="cardContent">
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            Github
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
-      <Card className="cardContainer">
-        <CardMedia
-          className="cardMedia"
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        <CardContent className="cardContent">
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
-      <Card className="cardContainer">
-        <CardMedia
-          className="cardMedia"
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        <CardContent className="cardContent">
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
+      <div className="row">
+        <div className="thumb-box">
+          <a href="">
+            <img src="https://source.unsplash.com/1600x900/?lama&peru" alt="" />
+            <span className="overlay-box">
+              <span className="meta">Team Project</span>
+              <span className="main-title">ShareBook</span>
+              <span className="description">
+                지역 내 중고책 대여 모바일 서비스
+              </span>
+              <button className="projectButton">Detail</button>
+              <button className="projectButton">github</button>
+            </span>
+          </a>
+        </div>
+
+        <div className="thumb-box">
+          <a href="">
+            <img src="https://source.unsplash.com/1600x900/?llama" alt="" />
+            {/* <img src="https://sharebook-gifs.s3.ap-northeast-2.amazonaws.com/signUp.gif" alt="" /> */}
+            <span className="overlay-box">
+              <span className="meta">Team Project</span>
+              <span className="main-title">CheerLeader</span>
+              <span className="description">랜덤 응원메세지 웹 서비스</span>
+              <button className="projectButton">Detail</button>
+              <button className="projectButton">github</button>
+            </span>
+          </a>
+        </div>
+
+        <div className="thumb-box">
+          <a href="">
+            <img src="https://source.unsplash.com/1600x900/?lama" alt="" />
+            <span className="overlay-box">
+              <span className="meta">Solo Project</span>
+              <span className="main-title">LamaChat</span>
+              <span className="description">실시간 채팅 웹 서비스</span>
+              <button className="projectButton">Detail</button>
+              <button className="projectButton">github</button>
+            </span>
+          </a>
+        </div>
+      </div>
     </div>
   </Layout>
 )
